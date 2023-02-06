5d4136637368c90e7ffe1c114801ade3cff52da5
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-02-07 00:07:23 +0500

    7.4

---
+++ AKibanov7.4.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void symbol(char *str, char s)
+{
+	int len=strlen(str);
+	str[0]=s;
+	str[len-1]=s;
+}
+
+int main()
+{
+	char str[100];
+	char s;
+	printf("Enter the string:\n");
+	gets(str);
+	printf("Enter the symbol:\n");
+	s=getchar();
+	symbol(str,s);
+	printf("Modified string:\n%s\n", str);
+	return 0;
+	
+//Во втором задании не понял как сделать работающую функцию,
+//поэтому мне достаточно только одного выполненного задания. 
+}
